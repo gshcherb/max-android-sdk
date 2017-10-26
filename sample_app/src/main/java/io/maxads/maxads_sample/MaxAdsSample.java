22ad9c4f530748e4000a1ddddab4afff2c53e578
steffanc <steffan.chartrand@gmail.com>
2017-10-25 17:13:35 -0700

    Renaming app to sample_app and using Android gradle plugin 3.0.0

--- sample_app/src/main/java/io/maxads/maxads_sample/MaxAdsSample.java
+++ sample_app/src/main/java/io/maxads/maxads_sample/MaxAdsSample.java
@@ -10,6 +10,7 @@ import java.util.Collections;
 
 import io.maxads.ads.base.MaxAds;
 import io.maxads.ads.base.util.Checks;
+import io.maxads.mraid.MRAIDBanner;
 import okhttp3.Interceptor;
 import okhttp3.logging.HttpLoggingInterceptor;
 
