7d645cbc5ad739c8f944a7338fb177f80a7959b5
steffanc <steffan.chartrand@gmail.com>
2017-10-30 16:36:29 -0700

    Session depth counter resets after app being backgrounded for 30s

--- sdk/src/main/java/io/maxads/ads/base/SessionDepthManager.java
+++ sdk/src/main/java/io/maxads/ads/base/SessionDepthManager.java
@@ -6,29 +6,35 @@ import android.support.annotation.NonNull;
 import com.jenzz.appstate.AppState;
 import com.jenzz.appstate.adapter.rxjava2.RxAppStateMonitor;
 
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import io.reactivex.functions.Consumer;
 
 /**
  * Session depth starts at 0 and is incremented for each ad request attempt regardless of whether a response is received
- * or not. A single session is defined as the app being in the foreground. When the app is backgrounded, the session
- * depth counter starts over again.
+ * or not. When the app is backgrounded for more than 30 seconds, the session depth counter starts over again.
  */
 public class SessionDepthManager {
+  private static final long SESSION_RESET_THRESHOLD_MS = TimeUnit.SECONDS.toMillis(30);
   @NonNull private final AtomicInteger mSessionDepth;
+  private long mTimeBackgroundedMs;
 
   public SessionDepthManager(@NonNull final Application application) {
     mSessionDepth = new AtomicInteger(0);
     RxAppStateMonitor.monitor(application)
-    .subscribe(new Consumer<AppState>() {
-      @Override
-      public void accept(AppState appState) throws Exception {
-        if (appState == AppState.BACKGROUND) {
-          mSessionDepth.set(0);
+      .subscribe(new Consumer<AppState>() {
+        @Override
+        public void accept(AppState appState) throws Exception {
+          if (appState == AppState.BACKGROUND) {
+            mTimeBackgroundedMs = System.currentTimeMillis();
+          } else if (appState == AppState.FOREGROUND) {
+            if(System.currentTimeMillis() - mTimeBackgroundedMs > SESSION_RESET_THRESHOLD_MS) {
+              mSessionDepth.set(0);
+            }
+          }
         }
-      }
-    });
+      });
   }
 
   public int getSessionDepth() {
